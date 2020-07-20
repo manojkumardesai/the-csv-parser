34a6f396d2b71577e71bb1280efcd7d7202d9d54
manojkumardesai <manojkumardbe@gmail.com>
2020-07-20 12:50:59 +0530

    csv to json convertion

--- the-csv-parser.js
+++ the-csv-parser.js
@@ -62,7 +62,7 @@ function processFile(inputStream) {
     var targetStream;
     var upperCaseTr = new Transform({
         transform(chunk, encoding, callback) {
-            this.push(chunk.toString().toUpperCase());
+            this.push(csvToJson(chunk.toString()));
             callback();
         }
     });
@@ -73,4 +73,28 @@ function processFile(inputStream) {
         targetStream = fs.createWriteStream(OUTPATH);
     }
     outStream.pipe(targetStream);
+}
+
+function csvToJson(csv) {
+    var lines = csv.split("\n");
+
+    var result = [];
+
+    var headers = lines[0].split(",");
+
+    for (var i = 1; i < lines.length; i++) {
+
+        var obj = {};
+        var currentline = lines[i].split(",");
+
+        for (var j = 0; j < headers.length; j++) {
+            obj[headers[j]] = currentline[j];
+        }
+
+        result.push(obj);
+
+    }
+
+    //return result; //JavaScript object
+    return JSON.stringify(result); //JSON
 }
